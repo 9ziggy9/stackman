a93cf6cad6b1bc27cbef3af37e6e49780d058c53
9ziggy9 <davidarogers@protonmail.com>
2023-03-22 17:41:46 -0700

    simple movement and key handling

--- main.py
+++ main.py
@@ -2,17 +2,7 @@ import pygame
 import config
 import animate
 
-fr_run = 0
-fr_icrouch = 0
-fr_idle = 0
-fr_jump = 0
-fr_land = 0
-fr_ledge = 0
-fr_pull = 0
-fr_push = 0
-fr_roll = 0
-fr_spin = 0
-fr_sneak = 0
+frame_idx = 0
 animation_timer = 0
 
 # TIME
@@ -26,62 +16,72 @@ pygame.display.set_caption("stackman -- dev")
 # font for FPS monitoring
 font = pygame.font.SysFont("Arial", 18)
 
+BTNS_UP = {119, 1073741906}
+BTNS_LEFT = {97, 1073741904}
+BTNS_DOWN = {115, 1073741905}
+BTNS_RIGHT = {100, 1073741903}
+
+ACTION_IDLE = 0
+ACTION_RUN = 1
+ACTION_PUSH = 2
+ACTION_PULL = 3
+
+DIR_RIGHT = 0
+DIR_LEFT = 1
+
+def stackman_init():
+    return {
+        "action": ACTION_IDLE,
+        "direction": DIR_RIGHT,
+        "position": pygame.math.Vector2((0,0))
+    }
+
+def set_animation_frames(sm):
+    if (sm["action"] == ACTION_RUN):
+        (sprite, frames) = animate.run
+        if (sm["direction"] == DIR_LEFT):
+            return (pygame.transform.flip(sprite, True, False),
+                    frames)
+        return (animate.run[0], animate.run[1])
+    if (sm["action"] == ACTION_IDLE):
+        frame_idx = 0
+        return (animate.idle[0], animate.idle[1])
+
+sm = stackman_init()
+
 running = True
 while running:
-    # event handling
+    (sprite, frames) = (None, None)
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
+        if e.type == pygame.KEYUP:
+            frame_idx = 0
+            sm["action"] = ACTION_IDLE
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+        sm["position"] += (10,0)
+        sm["direction"] = DIR_RIGHT
+        sm["action"] = ACTION_RUN
+    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        sm["position"] -= (10,0)
+        sm["direction"] = DIR_LEFT
+        sm["action"] = ACTION_RUN
 
     animation_timer += 1 / config.FPS
 
-    (run_sprite,     run_frames)     = animate.run
-    (idle_sprite,    idle_frames)    = animate.idle
-    (icrouch_sprite, icrouch_frames) = animate.icrouch
-    (jump_sprite,    jump_frames)    = animate.jump
-    (land_sprite,    land_frames)    = animate.land
-    (ledge_sprite,   ledge_frames)   = animate.ledge
-    (pull_sprite,    pull_frames)    = animate.pull
-    (push_sprite,    push_frames)    = animate.push
-    (roll_sprite,    roll_frames)    = animate.roll
-    (spin_sprite,    spin_frames)    = animate.spin
-    (sneak_sprite,   sneak_frames)   = animate.sneak
+    (sprite, frames) = set_animation_frames(sm)
 
     if animation_timer >= config.ANIMATION_SPEED:
-        fr_run = (fr_run + 1) % len(run_frames)
-        # fr_icrouch = (fr_icrouch + 1) % len(icrouch_frames)
-        # fr_idle = (fr_idle + 1) % len(idle_frames)
-        # fr_jump = (fr_jump + 1) % len(jump_frames)
-        # fr_land = (fr_land + 1) % len(land_frames)
-        # fr_ledge = (fr_ledge + 1) % len(ledge_frames)
-        # fr_pull = (fr_pull + 1) % len(pull_frames)
-        # fr_push = (fr_push + 1) % len(push_frames)
-        # fr_roll = (fr_roll + 1) % len(roll_frames)
-        # fr_spin = (fr_spin + 1) % len(spin_frames)
-        # fr_sneak = (fr_sneak + 1) % len(sneak_frames)
+        frame_idx = (frame_idx + 1) % len(frames)
         animation_timer = 0
 
     display.fill((0,0,0)) # color in RGB
-    frame_run = pygame.Rect(run_frames[fr_run])
-    # frame_icrouch = pygame.Rect(icrouch_frames[fr_icrouch])
-    # frame_idle = pygame.Rect(idle_frames[fr_idle])
-    # frame_jump = pygame.Rect(jump_frames[fr_jump])
-    # frame_land = pygame.Rect(land_frames[fr_land])
-    # frame_ledge = pygame.Rect(ledge_frames[fr_ledge])
-    # frame_pull = pygame.Rect(pull_frames[fr_pull])
-    # frame_push = pygame.Rect(push_frames[fr_push])
-    # frame_roll = pygame.Rect(roll_frames[fr_roll])
-    # frame_spin = pygame.Rect(spin_frames[fr_spin])
-    # frame_sneak = pygame.Rect(sneak_frames[fr_sneak])
-    display.blit(run_sprite, (0, 0), frame_run)
-    # display.blit(icrouch_sprite, (0, 48 * 5), frame_icrouch)
-    # display.blit(idle_sprite, (0, 48 * 5 * 2), frame_idle)
-    # display.blit(jump_sprite, (0, 48 * 5 * 3), frame_jump)
-    # display.blit(land_sprite, (48 * 5, 0), frame_land)
-    # display.blit(ledge_sprite, (48 * 5, 48 * 5), frame_ledge)
-    # display.blit(pull_sprite, (48 * 5, 48 * 5 * 2), frame_pull)
-    # display.blit(push_sprite, (48 * 5, 48 * 5 * 3), frame_push)
-    # display.blit(sneak_sprite, (48 * 5 * 2, 0), frame_sneak)
+    if frame_idx > len(frames) - 1:
+        frame_idx = 0
+    frames = pygame.Rect(frames[frame_idx])
+    display.blit(sprite, sm["position"], frames)
 
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
